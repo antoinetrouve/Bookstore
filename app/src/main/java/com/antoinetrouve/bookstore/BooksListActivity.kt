10ad77dacfc2a814f5f9a4caa2016aca648339e5
Antoine Trouvé <antrouve.pro@gmail.com>
2019-01-03 15:53:40 +0100

    init bookstore kotlin project

---
+++ app/src/main/java/com/antoinetrouve/bookstore/BooksListActivity.kt
@@ -0,0 +1,12 @@
+package com.antoinetrouve.bookstore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BooksListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_list)
+    }
+}
