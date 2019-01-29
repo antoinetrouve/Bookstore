9929c3e28125b5403c8166aee1717533c5a5554d
Antoine Trouvé <antrouve.pro@gmail.com>
2019-01-29 09:03:41 +0100

    fix androidx dependencies

--- app/src/main/java/com/antoinetrouve/bookstore/BooksListActivity.kt
+++ app/src/main/java/com/antoinetrouve/bookstore/BooksListActivity.kt
@@ -1,7 +1,7 @@
 package com.antoinetrouve.bookstore
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class BooksListActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/antoinetrouve/bookstore/repository/SyncRepositoryWorker.kt
@@ -0,0 +1,21 @@
+package com.antoinetrouve.bookstore.repository
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.antoinetrouve.bookstore.App
+import timber.log.Timber
+
+class SyncRepositoryWorker(context : Context, params: WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        Timber.i("Synchronizing books ... ")
+
+        val bookApi = FakeBookApi()
+        val bookDao = App.db.bookDao()
+
+        bookDao.insertBooks(bookApi.loadBooks())
+
+
+        return Result.success()
+    }
+}
