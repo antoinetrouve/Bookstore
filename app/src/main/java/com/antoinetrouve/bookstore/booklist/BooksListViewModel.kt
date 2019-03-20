6908b0d98432dd0c664858ec21c83c651e4d8d01
antoinetrouve <antoinetrouve.france@gmail.com>
2019-03-20 17:36:15 +0100

    create book list adapter)

--- app/src/main/java/com/antoinetrouve/bookstore/booklist/BooksListViewModel.kt
+++ app/src/main/java/com/antoinetrouve/bookstore/booklist/BooksListViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.antoinetrouve.bookstore.App
 import com.antoinetrouve.bookstore.Book
 
-class BookListViewModel : ViewModel() {
+class BooksListViewModel : ViewModel() {
 
     // Todo pass App into constructor parameter
     val books: LiveData<List<Book>> = App.db.bookDao().getAllBooks()
