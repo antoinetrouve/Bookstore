2831008f8866b671f3fbaf620331c322f70abb3c
antoinetrouve <antoinetrouve.france@gmail.com>
2019-03-21 12:02:41 +0100

    attach book detail activity with viewModel and his factory

--- app/src/main/java/com/antoinetrouve/bookstore/bookdetail/BookDetailActivity.kt
+++ app/src/main/java/com/antoinetrouve/bookstore/bookdetail/BookDetailActivity.kt
@@ -3,6 +3,8 @@ package com.antoinetrouve.bookstore.bookdetail
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders
+import com.antoinetrouve.bookstore.Book
 import com.antoinetrouve.bookstore.R
 
 import kotlinx.android.synthetic.main.activity_book_detail.*
@@ -22,10 +24,11 @@ class BookDetailActivity : AppCompatActivity() {
         val bookId = intent.getIntExtra(EXTRA_BOOK_ID, 1)
         Timber.d("Book id = $bookId")
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        // Pass book id when attach viewModel to activity without call constructor directly to keep
+        // separate view and model
+        val factory = BookDetailViewModelFactory(bookId)
+        ViewModelProviders.of(this, factory).get(BookDetailViewModel::class.java)
+
     }
 
 }
